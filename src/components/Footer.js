e2390956441a354ea618b015e806f83eb12d54e0
taufiksty <taufik.satya09@gmail.com>
2022-12-24 20:37:02 +0700

    make simple white page

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; 2022 Satya's development. All rights reserved.</small>
+    </footer>
+  );
+}
