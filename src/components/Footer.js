079b1f222a8cd44f815fd3a886aca8e2ba01dbef
Muhamad Taufik Satya <77332356+taufiksty@users.noreply.github.com>
2022-12-25 16:16:45 +0700

    Delete Footer.js

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>&copy; 2022 Satya's development. All rights reserved.</small>
-    </footer>
-  );
-}
