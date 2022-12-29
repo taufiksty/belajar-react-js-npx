6de5d8c7dfe1b01690fed32cfb65f39944ee15ae
taufiksty <taufik.satya09@gmail.com>
2022-12-29 07:43:14 +0700

    bug

--- src/components/ProjectCardProfile/Info.js
+++ src/components/ProjectCardProfile/Info.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Foto from "../../images/ronaldo.jpg";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Info() {
   return (
     <div className="info">
-      <img src={Foto} alt="ronaldo" className="info--img"/>
+      <img src={Foto} alt="ronaldo" className="info--img" />
       <h2 className="info--name">Cristiano Ronaldo</h2>
       <p className="info--title">Forward</p>
       <p className="info--detail">ronaldo.website</p>
-      <div>
-        <button>Email</button>
-        <button>Linkedin</button>
+      <div className="button">
+        {/* <button className="button--email">
+          <FontAwesomeIcon icon={faEnvelope} />
+          Email
+        </button>
+        <button className="button--linkedin">
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+          Linkedin
+        </button> */}
       </div>
     </div>
   );
