5a423838993c24f6deb7271beb0d21ba404d41ca
taufiksty <taufik.satya09@gmail.com>
2022-12-29 13:07:52 +0700

    card

--- src/components/ProjectCardProfile/Info.js
+++ src/components/ProjectCardProfile/Info.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Foto from "../../images/ronaldo.jpg";
 
 export default function Info() {
   return (
-    <h1>Info goes here</h1>
+    <div className="info">
+      <img src={Foto} alt="ronaldo" className="info--img"/>
+      <h2 className="info--name">Cristiano Ronaldo</h2>
+      <p className="info--title">Forward</p>
+      <p className="info--detail">ronaldo.website</p>
+      <div>
+        <button>Email</button>
+        <button>Linkedin</button>
+      </div>
+    </div>
   );
 }
