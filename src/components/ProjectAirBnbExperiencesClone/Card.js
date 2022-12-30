7bc9da9345d71afd3857766785b3e2eed9b05331
taufiksty <taufik.satya09@gmail.com>
2022-12-30 20:35:57 +0700

    AirBnb props

--- src/components/ProjectAirBnbExperiencesClone/Card.js
+++ src/components/ProjectAirBnbExperiencesClone/Card.js
@@ -1,9 +1,24 @@
 import React from "react";
+import CardPhoto from "../../images/card-photo.png";
+import Star from "../../images/star.svg";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <section className="card">
-      <h1>Hello, this is card</h1>
+      <div className="card--container">
+        <div className="card--item">
+          <img src={CardPhoto} alt="katie-zaferes" className="card--photo"></img>
+          <div className="card--status">
+            SOLD OUT
+          </div>
+          <div className="card--star">
+            <img src={Star} alt=""></img>
+            <p>5.0 <span className="star--number">(6) | USA</span></p>
+          </div>
+          <p className="card--detail">Life lessons with Katie Zaferes</p>
+          <p className="card--detail"><span className="bold">From $136</span> / person</p>
+        </div>
+      </div>
     </section>
   );
 }
