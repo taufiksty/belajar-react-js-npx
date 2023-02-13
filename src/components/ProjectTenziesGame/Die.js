c5e39362f82016d001fe0a4ab7cef949dc2d578f
taufiksty <taufik.satya09@gmail.com>
2023-02-13 21:29:15 +0700

    puts real dots on dice

--- src/components/ProjectTenziesGame/Die.js
+++ src/components/ProjectTenziesGame/Die.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+	const dots = Array(props.value).fill("•").join(" ");
 	return (
-		<div className={`die--box ${props.isHeld === true ? "is-held" : ""}`} onClick={props.holdDice}>
-			{props.value}
+		<div
+			className={`die--box ${props.isHeld === true ? "is-held" : ""}`}
+			onClick={props.holdDice}>
+			<div className={props.value === 4 ? "dot dot-four" : "dot"}>{dots}</div>
 		</div>
 	);
 }
